0c8d7e5b2a35ed954649bdb2b790b6b21363f25b
Charanjeet97 <CharanjeetJaswani@Pixent-Interactive.com>
2023-06-25 15:02:00 +0530

    solved black screen and anchor problem

--- Assets/PlaneDetection/Scripts/PlaceOnPlane.cs
+++ Assets/PlaneDetection/Scripts/PlaceOnPlane.cs
@@ -10,9 +10,9 @@ public class PlaceOnPlane : MonoBehaviour
     private ARPlaneManager planeManager;
     private ARRaycastManager raycastManager;
     private ARAnchorManager anchorManager;
-    private GameObject placeOnPlaneObject;
     private ARSessionOrigin sessionOrigin;
     private Camera arCamera;
+    private GameObject placeOnPlaneObject;
     [SerializeField] private GameObject placeOnPlaneObjectPrefab;
     [SerializeField] private GameObject placementIndicator;
     [SerializeField] private float minPlaneSize;
@@ -46,28 +46,40 @@ public class PlaceOnPlane : MonoBehaviour
             if (raycastManager.Raycast(ray, hits, TrackableType.PlaneWithinPolygon) && MeasurePlane(out ARPlane plane))
             {
                 pose = hits[0].pose;
-                plane = (ARPlane)hits[0].trackable;
+                arPlane = planeManager.GetPlane(hits[0].trackableId);
                 Quaternion rotation = Quaternion.LookRotation(Vector3.forward, hits[0].pose.up);
                 placementIndicator.SetActive(true);
                 sessionOrigin.MakeContentAppearAt(placementIndicator.transform, pose.position, rotation);
+                if (placementIndicator.activeInHierarchy && Input.touchCount > 0) 
+                {
+                    Touch touch = Input.GetTouch(0);
+                    switch (touch.phase)
+                    {
+                        case TouchPhase.Began:
+                            PlaceARObjectOnPlane(pose,arPlane);
+                            break;
+                        case TouchPhase.Ended or TouchPhase.Canceled :
+                            Debug.Log("Touch ended");
+                            break;
+                    }
+                }
             }
 
-            if (placementIndicator.activeInHierarchy && Input.GetMouseButton(0))
-            {
-                PlaceARObjectOnPlane(pose,arPlane);
-            }
         }
     }
 
     private void PlaceARObjectOnPlane(Pose pose,ARPlane plane)
     {
         Vector3 position = pose.position;
-        Quaternion lookRotation = pose.rotation;
+        Vector3 direction = arCamera.transform.position - pose.position;
+        direction.y = 0f;
+        Quaternion lookRotation = Quaternion.LookRotation(direction, Vector3.up);
         ARAnchor anchor = anchorManager.AttachAnchor(plane, pose);
         placeOnPlaneObject = Instantiate(placeOnPlaneObjectPrefab,anchor.transform);
-        placeOnPlaneObject.transform.localScale = Vector3.one * scaleFactor; 
+        placeOnPlaneObject.transform.localScale = Vector3.one * scaleFactor;
+        placementIndicator.SetActive(false);
         TogglePlanes(false);
-        sessionOrigin.MakeContentAppearAt(anchor.transform, position, lookRotation);
+        sessionOrigin.MakeContentAppearAt(placeOnPlaneObject.transform, position, lookRotation);
     }
     
     private bool MeasurePlane(out ARPlane arPlane)
